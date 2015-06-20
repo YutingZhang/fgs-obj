8ffbe5a17c45b69d2339cc335eab747dbbde8786
YutingZhang <ritchie.zyt@gmail.com>
2015-06-20 15:50:51 -0400

    separate cnn and the rest of the model for downloading; add trainval-categlist.mat

--- get_data.sh
+++ get_data.sh
@@ -2,15 +2,19 @@
 
 echo Get Trained Model
 
-wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/voc2007_train_cache.tar.gz
-tar -xzf voc2007_train_cache.tar.gz
-rm -f voc2007_train_cache.tar.gz
+wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/voc2007_train_cache.cnn.tar.gz
+tar -xzf voc2007_train_cache.cnn.tar.gz
+#rm -f voc2007_train_cache.cnn.tar.gz
+
+wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/voc2007_train_cache.cnn.tar.gz
+tar -xzf voc2007_train_cache.rest.tar.gz
+#rm -f voc2007_train_cache.rest.tar.gz
 
 echo Get Orignal VGG Model
 
 wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/cnn_finetuning.tar.gz
 tar -xzf cnn_finetuning.tar.gz
-rm -f cnn_finetuning.tar.gz
+#rm -f cnn_finetuning.tar.gz
 
 
 echo ******************************************************************
