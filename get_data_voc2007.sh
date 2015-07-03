9ee869770fcd48a4f2e1389a0ababfcd0cc6263a
yutingzh <yutingzh@umich.edu>
2015-07-02 23:02:50 -0400

    fix wrong links in get_data_voc2007.sh and get_data_voc2012.sh

--- get_data_voc2007.sh
+++ get_data_voc2007.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 echo Get Trained Model
-
 wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/voc2007_train_cache.cnn.tar.gz
+echo ">>>" Decompressing
 tar -xzf voc2007_train_cache.cnn.tar.gz
-#rm -f voc2007_train_cache.cnn.tar.gz
+echo ">>>" Done
 
-wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/voc2007_train_cache.cnn.tar.gz
+wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/voc2007_train_cache.rest.20150620.tar.gz
 tar -xzf voc2007_train_cache.rest.20150620.tar.gz
-#rm -f voc2007_train_cache.rest.tar.gz
+echo ">>>" Decompressing
+echo ">>>" Done
 
 echo Get Orignal VGG Model
 
 wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2007/cnn_finetuning.tar.gz
+echo ">>>" Decompressing
 tar -xzf cnn_finetuning.tar.gz
-#rm -f cnn_finetuning.tar.gz
+echo ">>>" Done
 
 
-echo ******************************************************************
-echo * Please Get VOC 2007 data by yourself and put it into ./voc2007 *
-echo ******************************************************************
+echo "******************************************************************"
+echo "* Please Get VOC 2007 data by yourself and put it into ./voc2007 *"
+echo "******************************************************************"
 
 
--- get_data_voc2012.sh
+++ get_data_voc2012.sh
@@ -3,12 +3,13 @@
 echo Get Trained Model
 
 wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2012/voc2012_train_cache.cnn.tar.gz
+echo ">>> Decompressing"
 tar -xzf voc2012_train_cache.cnn.tar.gz
-#rm -f voc2012_train_cache.cnn.tar.gz
+echo "Done"
 
-wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2012/voc2012_train_cache.cnn.tar.gz
+wget -c http://www.ytzhang.net/files/fgs-obj/cache/voc2012/voc2012_train_cache.rest.tar.gz
+echo ">>> Decompressing"
 tar -xzf voc2012_train_cache.rest.tar.gz
-#rm -f voc2012_train_cache.rest.tar.gz
-
+echo "Done"
 
 
